0f81fbe76906eefc2dc6b5b2904ef12a111fe16d
ka22ka10 <44335034+ka22ka10@users.noreply.github.com>
2020-03-12 18:04:11 +0200

    shaders

--- shaders/fshader_color.glsl
+++ shaders/fshader_color.glsl
@@ -2,29 +2,46 @@
 
 struct Material
 {
-	sampler2D textureMap;
-	// You can add more fields here...
-	// Such as:
-	//		1. diffuse/specular relections constants
-	//		2. specular alpha constant
-	//		3. Anything else you find relevant
+	vec3 color;
+	float KA;
+	float KD;
+	float KS;
+	float shine;
 };
 
-// We set this field's properties from the C++ code
-uniform Material material;
 
+uniform vec3 lightPos[5];
+uniform vec3 lightCol[5];
+uniform vec3 eye;
+
+uniform Material material;
 // Inputs from vertex shader (after interpolation was applied)
 in vec3 fragPos;
 in vec3 fragNormal;
-in vec2 fragTexCoords;
 
-// The final color of the fragment (pixel)
 out vec4 frag_color;
 
 void main()
 {
-	// Sample the texture-map at the UV coordinates given by 'fragTexCoords'
-	vec3 textureColor = vec3(texture(material.textureMap, fragTexCoords));
+	vec3 IA = material.color * material.KA;
+	vec3 ID = vec3(0.0f);
+	vec3 IS = vec3(0.0f);
+	for (int i=0; i<5; i++)
+	{
+		// diffuse 
+		vec3 norm = normalize(fragNormal);
+		vec3 lightDir = normalize(lightPos[i] - fragPos);
+		float diff = max(dot(norm, lightDir), 0.0) * material.KD;
+		ID = ID + diff * lightCol[i];
 
-	frag_color = vec4(textureColor, 1.0f);
+		// specular
+		vec3 viewDir = normalize(eye - fragPos);
+		vec3 reflectDir = reflect(-lightDir, norm);  
+		float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16) * material.KS;
+		IS = IS + spec * lightCol[i];
+	}
+	vec3 I = IA + (ID + IS);
+
+	frag_color = vec4(I,1.0f);
+	
 }
